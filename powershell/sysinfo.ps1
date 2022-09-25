ad20db0e28d956a5a2a48d830c8977b324dccdd6
warnerbl <warnerbl@mail.uc.edu>
2022-09-25 11:38:15 -0400

    Initial commit

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -1,3 +1,9 @@
-﻿function getIP {
-    (Get-NetIPAddress).IPv4Address | Select-String "192*"
-}
+﻿
+$IP = getIP
+$USER = getUser
+$HOSTNAME = getHost
+$POWERSHELL = getPowerShellVersion
+$DATE = getDate
+
+
+Write-Host("This machine's IP is $IP. User is $USER. Hostname is $HOSTNAME. PowerShell Version $POWERSHELL. Today's date is $DATE")
