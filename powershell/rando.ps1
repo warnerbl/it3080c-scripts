c07a52ee4f0c483f43a20bfae6be8c4e00518b14
warnerbl <warnerbl@mail.uc.edu>
2022-10-17 13:44:30 -0400

    lab 7

---
+++ powershell/rando.ps1
@@ -0,0 +1,8 @@
+﻿$RANDO = 0
+$LogFile = "C:\logs\rando.log"
+
+for($i=0; $i -lt 5; $i++) {
+    $RANDO=Get-Random -Maximum 1000 -Minimum 1
+    Write-Host($RANDO) 
+    Add-Content $LogFile "INFO: Random number is ${RANDO}"
+} 
---
+++ powershell/tools.psm1
@@ -0,0 +1,4 @@
+﻿Function GetIP {
+   $IP = Get-NetIPAddress | Where-Object {$_.IPv4Address -like '192*'}
+   return $IP.IPv4Address
+}
